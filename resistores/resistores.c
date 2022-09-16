d44f8c9c2b998f47bb54e28c3706647393103e21
orlando figueiredo <orlando.a.figueiredo@unesp.br>
2022-09-16 20:46:37 -0300

    Conversão de números

--- resistores/resistores.c
+++ resistores/resistores.c
@@ -28,14 +28,23 @@ int tabela (char* cor){
 }
 
 int main(){
-    int n1, n2, n3;
+    int n1, n2, n3, i, res;
+    bool achou;
     char cor1[100], cor2[100], cor3[100];
     char fica;
     int combs[13][2]={{1,0}, 
                       {1,2}, 
                       {1,5},
                       {1,8},
-                      {2,2}};
+                      {2,2},
+                      {2,7},
+                      {3,3},
+                      {3,9},
+                      {4,7},
+                      {5,6},
+                      {6,8},
+                      {8,2},
+                      {9,1}};
 
     do{
         printf("Digite uma cor: ");
@@ -46,10 +55,32 @@ int main(){
         scanf("%s", &cor2[0]);
         printf("%d\n", tabela(cor2));
         n2 = tabela(cor2);
+
+        achou= false;
+        for(i=0; i<13; i++){
+            if(n1==combs[i][0] && n2==combs[i][1]){
+                achou=true;
+            }
+        } 
+        if (!achou){
+            printf("Essa combinação não existe!!\n\n");
+            continue;
+        }
+        res = n1*10 + n2;
+        printf("%d\n", res);
+
         printf("Digite a terceira cor: ");
         scanf("%s", &cor3[0]);
         printf("%d\n", tabela(cor3));
         n3 = tabela(cor3);
+        for(i=0; i<n3; i++){
+            res = res*10;
+        }
+
+        printf("%d\n", res);
+
+
+
         printf("Quer continuar? (S / N)\n");
         scanf(" %c", &fica);
 
