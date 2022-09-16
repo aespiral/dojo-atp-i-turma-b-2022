51662cc800e5601ef26c8089fa45d86d5ca7d64a
orlando figueiredo <orlando.a.figueiredo@unesp.br>
2022-09-16 20:16:48 -0300

    Tabela de Cores

--- resistores/resistores.c
+++ resistores/resistores.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+int tabela (char* cor){
+    if (strcmp(cor,"Preto") == 0 )
+        return 0;
+    if (strcmp(cor,"Marrom") == 0)
+        return 1;
+    if (strcmp(cor,"Vermelho") == 0 )
+        return 2;
+    if (strcmp(cor,"Laranja") == 0 )
+        return 3;
+    if (strcmp(cor,"Amarelo") == 0 )
+        return 4;   
+    if (strcmp(cor,"Verde") == 0 )
+        return 5;    
+    if (strcmp(cor,"Azul") == 0 )
+        return 6; 
+    if (strcmp(cor,"Violeta") == 0 )
+        return 7;
+    if (strcmp(cor,"Cinza") == 0 )
+        return 8;
+    if (strcmp(cor,"Branco") == 0 )
+        return 9;
+    if (strcmp(cor,"Dourado") == 0 )
+        return -1;
+}
 
 int main(){
     char cor1[100], cor2[100], cor3[100];
     char fica;
+    int combs[13][2]={{1,0}};
 
     do{
         printf("Digite uma cor: ");
         scanf("%s", &cor1[0]);
-        printf("%s\n", cor1);
+        printf("%d\n", tabela(cor1));
         printf("Digite a segunda cor: ");
         scanf("%s", &cor2[0]);
-        printf("%s\n", cor2);
+        printf("%d\n", tabela(cor2));
         printf("Digite a terceira cor: ");
         scanf("%s", &cor3[0]);
-        printf("%s\n", cor3);
+        printf("%d\n", tabela(cor3));
         printf("Quer continuar? (S / N)\n");
         scanf(" %c", &fica);
 
-    }while(fica != 'n' || fica != 'N');
+
+    }while(fica != 'n' && fica != 'N');
 
     return 0;
 }
